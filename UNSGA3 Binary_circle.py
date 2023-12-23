773b95d6a189ac29633c2c8b6668de8ad781be9a
faaatigue <63487470+faaatigue@users.noreply.github.com>
2023-12-23 16:30:55 +0800

    New

--- UNSGA3 Binary_circle.py
+++ UNSGA3 Binary_circle.py
@@ -1,15 +1,17 @@
-import numpy as np
-from pymoo.core.problem import ElementwiseProblem
-from numpy import sin, cos, max, pi, exp, sqrt, abs, nanmax, nan
-from matplotlib import pyplot as plt
 import time
-from pymoo.algorithms.moo.unsga3 import UNSGA3
+import copy
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from multiprocessing.pool import ThreadPool
+from numpy import sin, cos, pi, exp, sqrt, abs, nanmax, nan, trapz
 from pymoo.optimize import minimize
-from pymoo.operators.sampling.rnd import BinaryRandomSampling, FloatRandomSampling, IntegerRandomSampling
+from pymoo.algorithms.moo.unsga3 import UNSGA3
+from pymoo.core.problem import ElementwiseProblem, StarmapParallelization
+from pymoo.operators.sampling.rnd import BinaryRandomSampling
 from pymoo.operators.mutation.bitflip import BitflipMutation
 from pymoo.operators.crossover.pntx import TwoPointCrossover
 from pymoo.util.ref_dirs import get_reference_directions
-from pymoo.visualization.scatter import Scatter
 
 
 
@@ -35,7 +37,7 @@ D = M * dx
 L_1 = 2 * D ** 2 / wavelength
 
 F_D = 0.47
-L = wavelength * 10 * F_D
+L = D * F_D
 
 # The distance between the transmitter and the center of the RIS
 d1 = L  
@@ -45,6 +47,10 @@ phi_t = 0
 d2 = 100  
 theta_r = np.radians(0)
 phi_r = np.radians(0)
+
+Theta_hp = np.radians(10)
+Theta_fn = np.radians(13)
+SLL = -20
 
 
 def power_radiation_pattern_t(theta, phi):
@@ -117,28 +123,27 @@ s = 1 - u_**2 - v_**2
 
 # The parameters settings for objective function
 u_0, v_0 = sin(theta_r) * cos(phi_r), sin(theta_r) * sin(phi_r)
-Theta_hp = np.radians(6)
-Theta_fn = np.radians(10)
-SLL = -30
+
 
 # Lower mask (HPBW)
-E_L = (u_ - u_0)**2 + (v_ - v_0)**2 - sin(Theta_hp)**2
+E_L = (u_ - u_0)**2 + (v_ - v_0)**2 - sin(Theta_hp / 2)**2
 
 # Upper mask (SLL)
-E_U = (u_ - u_0)**2 + (v_ - v_0)**2 - sin(Theta_fn)**2
-
+E_U = (u_ - u_0)**2 + (v_ - v_0)**2 - sin(Theta_fn / 2)**2
 
 
 class BinaryMatrixProblem(ElementwiseProblem):
 
-    def __init__(self, n_rows, n_cols):
+    def __init__(self, n_rows, n_cols, **kwargs):
         super(BinaryMatrixProblem, self).__init__(
             n_var=n_rows * n_cols,  # Number of variables
             n_obj=1,  # Number of objectives
-            n_constr=0,  # Number of constraints
+            # n_constr=1,  # Number of constraints
+            n_constr=0,
             xl=0,  # Variable lower bound (usually 0 for binary problems)
             xu=1,  # Variable upper bound (usually 1 for binary problems)
             vtype=int,
+            **kwargs
         )
         self.n_rows = n_rows
         self.n_cols = n_cols
@@ -165,32 +170,38 @@ class BinaryMatrixProblem(ElementwiseProblem):
             for m in range(0, M):
                 Gamma[n, m] = A * exp(1j * matrix[n, m])
         
-        bla_2 = (power_radiation_pattern_t(theta_tx_nm, 0) * \
-                 power_radiation_pattern_cell(theta_tx_nm, 0) / rmn_rf) * \
+        bla = (power_radiation_pattern_t(theta_t_nm, 0) * \
+                 power_radiation_pattern_cell(theta_t_nm, 0) / rmn_rf) * \
                  Gamma*  exp(-1j * k * rmn_rf) 
 
-        matrice = np.fft.ifft2(bla_2, (N_x, N_y))
+        matrice = np.fft.ifft2(bla, (N_x, N_y))
         matrice = np.fft.fftshift(matrice)
 
-        P_r1 = np.zeros((len(r_p_vect), len(r_q_vect)), dtype = 'complex_')
+        E_1 = np.zeros((len(r_p_vect), len(r_q_vect)), dtype = 'complex_')
         
         for p in range(0, len(r_p_vect)):
             for q in range(0, len(r_q_vect)): 
 
                 if s[p, q] < 0:
-                    P_r1[p, q] = nan
+                    E_1[p, q] = nan
                     u_[p, q], v_[p, q] = nan, nan
 
                 else: 
-                    P_r1[p, q] = sqrt(s[p, q]) * M * N * \
+                    E_1[p, q] = sqrt(s[p, q]) * M * N * \
                                  matrice[p, q]
                     
         
         # Normalize
-        P_r = P_r1 / nanmax(P_r1)
-        # P_r = P_r1
-        values = 10 * np.log10(abs(P_r))
-        # values = abs(P_r)
+        E_1 = abs(E_1)
+        E = E_1 / nanmax(E_1)
+        # E = E_1
+        values = 20 * np.log10(E) 
+        # values = abs(E)
+
+        for p in range(0, len(r_p_vect)):
+            for q in range(0, len(r_q_vect)): 
+                if values[p, q] < -100:
+                    values[p, q] = nan
                     
         # Calculate objective function
         # First objective function and Lower mask (HPBW)
@@ -198,7 +209,7 @@ class BinaryMatrixProblem(ElementwiseProblem):
 
         for p in range(0, len(r_p_vect)):
             for q in range(0, len(r_q_vect)): 
-                if E_L[p, q] <= 0 and values[p, q] < -3:
+                if E_L[p, q] <= 0 and values[p, q] < -3 and s[p, q] >= 0:
                     F_L.append((values[p, q] - (-3))**2)
 
         if len(F_L) == 0:
@@ -212,11 +223,8 @@ class BinaryMatrixProblem(ElementwiseProblem):
 
         for p in range(0, len(r_p_vect)):
             for q in range(0, len(r_q_vect)): 
-                if E_U[p, q] > 0 and values[p, q] > SLL:
-                    F_U.append((values[p, q] - SLL)**2)
-
-                elif E_U[p, q] <= 0 and values[p, q] > 0:
-                    F_U.append((values[p, q])**2)                        
+                if E_U[p, q] > 0 and values[p, q] > SLL and s[p, q] >= 0:
+                    F_U.append((values[p, q] - SLL)**2)                      
 
         if len(F_U) == 0:
             F_U.append(0)
@@ -224,35 +232,46 @@ class BinaryMatrixProblem(ElementwiseProblem):
         F_U_sum = sum(F_U) / len(F_U)**2
 
         F_obj = F_L_sum + F_U_sum
+        # F_obj = 2*F_L_sum + F_U_sum
 
+
+        # Calculate the constraint
+        # F_C = len(np.argwhere((E_C<=0) & (values==0)))
 
         # Calculate the fitness function 
         fitness = F_obj
-        print(fitness)
+        # print(fitness)
         out["F"] = fitness
         # out["F"] = [F_L_sum, F_U_sum]
+        # out["G"] = 1 - F_C
 
 
 
 # Parameters for optimization
 n_rows = N
 n_cols = M
-problem = BinaryMatrixProblem(n_rows, n_cols)
+
+# initialize the thread pool and create the runner
+pool = ThreadPool()
+runner = StarmapParallelization(pool.starmap)
+
+# define the problem by passing the starmap interface of the thread pool
+problem = BinaryMatrixProblem(n_rows, n_cols, elementwise_runner=runner)
 
 # selecttion = TournamentSelection()  
 # sampling = LHS()
-sampling = BinaryRandomSampling()
 # sampling = FloatRandomSampling()
 # sampling = IntegerRandomSampling()
 # crossover = BinomialCrossover(prob=0.9)  
 # mutation = GaussianMutation(prob=0.1)
+sampling = BinaryRandomSampling()
 crossover=TwoPointCrossover(prob=0.8)
 mutation=BitflipMutation(prob=0.4)
 
 # Configure the algorithm
 algorithm = UNSGA3(
-    pop_size=36,  # Population size
-    ref_dirs=get_reference_directions("energy", 1, 36),  # Number of reference directions
+    pop_size=1000,  # Population size
+    ref_dirs=get_reference_directions("energy", 1, 1000),  # Number of reference directions
     # selecttion=selecttion,
     sampling=sampling,
     crossover=crossover,
@@ -266,7 +285,7 @@ algorithm = UNSGA3(
 res = minimize(
     problem,
     algorithm,
-    # termination=("n_gen", 1000),
+    termination=("n_gen", 400),
     seed=1,
     save_history=True,
     verbose=True,
@@ -275,13 +294,25 @@ res = minimize(
 )
 
 
+print('Threads:', res.exec_time)
+
+pool.close()
+
 
 # Output the best fitness value for each generation
 best_fitness_each_gen = [gen.opt.get("F") for gen in res.history]
 for gen_num, fitness in enumerate(best_fitness_each_gen):
     print(f"Generation {gen_num + 1}: {fitness}")
 
-
+# Plot the best fitness values
+best_fitness_each_gen = np.array(best_fitness_each_gen)
+best_fitness_each_gen = best_fitness_each_gen.reshape(-1)
+plt.plot(range(1, len(best_fitness_each_gen) + 1), best_fitness_each_gen, marker='o')
+plt.xlabel('Generation')
+plt.ylabel('Best Fitness Value')
+plt.title('Evolution of Best Fitness Value over Generations')
+plt.grid(True)
+plt.show()
 
 # Output optimization results
 best_fitness = res.F
@@ -336,38 +367,62 @@ for n in range(0, N):
     for m in range(0, M):
         Gamma[n, m] = A * exp(1j * phi_Gamma[n, m])
 
-bla_2 = (power_radiation_pattern_t(theta_tx_nm, 0) * \
-            power_radiation_pattern_cell(theta_tx_nm, 0) / rmn_rf) * \
+bla = (power_radiation_pattern_t(theta_t_nm, 0) * \
+            power_radiation_pattern_cell(theta_t_nm, 0) / rmn_rf) * \
             Gamma*  exp(-1j * k * rmn_rf) 
 
-matrice = np.fft.ifft2(bla_2, (N_x, N_y))
+N_x = N*100
+N_y = M*100
+p_inf, p_sup = -(N_x - 1) / 2, (N_x - 1) / 2
+q_inf, q_sup = -(N_y - 1) / 2, (N_y - 1) / 2
+# p_inf, p_sup = 1, N_x
+# q_inf, q_sup = 1, N_y
+
+r_p_vect = np.arange(p_inf, p_sup + 1, 1)
+r_q_vect = np.arange(q_inf, q_sup + 1, 1)
+            
+
+u, v = 2 * pi * r_p_vect / (N_x * dx * k), \
+       2 * pi * r_q_vect / (N_y * dy * k)
+
+u_, v_ = np.meshgrid(u,v)
+
+s = 1 - u_**2 - v_**2
+
+matrice = np.fft.ifft2(bla, (N_x, N_y))
 matrice = np.fft.fftshift(matrice)
 
-P_r1 = np.zeros((len(r_p_vect), len(r_q_vect)), dtype = 'complex_')
+E_1 = np.zeros((len(r_p_vect), len(r_q_vect)), dtype = 'complex_')
 
 for p in range(0, len(r_p_vect)):
     for q in range(0, len(r_q_vect)): 
 
         if s[p, q] < 0:
-            P_r1[p, q] = nan
+            E_1[p, q] = nan
             u_[p, q], v_[p, q] = nan, nan
 
         else: 
-            P_r1[p, q] = sqrt(s[p, q]) * M * N * \
+            E_1[p, q] = sqrt(s[p, q]) * M * N * \
                             matrice[p, q]
             
 
 # Normalize
-P_r = P_r1 / nanmax(P_r1)
-# P_r = P_r1
-values = 10 * np.log10(abs(P_r))
-# values = abs(P_r)
+E_1 = abs(E_1)
+E = E_1 / nanmax(E_1)
+# E = E_1
+values = 20 * np.log10(E) 
+# values = abs(E)
 
 
 # Make sure plot correctly
-values1 = values
+values1 = copy.deepcopy(values)
+
 for p in range(0, len(r_p_vect)):
     for q in range(0, len(r_q_vect)): 
+        if values[p, q] < -100:
+            values[p, q] = nan
+            values1[p, q] = nan
+
         if values1[p, q] < -20.5:
             values1[p, q] = -20.5
 
@@ -383,9 +438,91 @@ plt.colorbar(label='')
 plt.axis('off')
 
 # Add a title
-plt.title('')
+plt.title('Normalized radiation patterns')
 
 # Display the plot
 plt.show()
 
 
+# Plot Area over -3dB
+values2 = copy.deepcopy(values1)
+
+values2[np.where(np.isclose(values2, 0, atol=1e-1))] = 0
+values2[np.where((values2 >= -3) & (values2 < 0))] = -1.5
+
+plt.imshow(values2, cmap='jet', origin='lower')
+plt.axis('equal') 
+plt.colorbar(label='')
+plt.axis('off')
+plt.title('Over -3dB Area')
+plt.show()
+
+
+# Display the masks 
+E_L = (u_ - u_0)**2 + (v_ - v_0)**2 - sin(Theta_hp / 2)**2
+E_U = (u_ - u_0)**2 + (v_ - v_0)**2 - sin(Theta_fn / 2)**2
+
+values3 = copy.deepcopy(values1)
+values3[np.where(np.isclose(values3, 0, atol=1e-2))] = -20
+values3[np.where((values3 >= -3) & (values3 < 0))] = -1.5
+values3[np.where(np.isclose(E_L, 0, atol=1e-4))] = -12.5
+values3[np.where(np.isclose(E_U, 0, atol=1e-3))] = 0
+
+plt.imshow(values3, cmap='jet', origin='lower')
+plt.axis('equal') 
+plt.colorbar(label='')
+plt.axis('off')
+plt.title('Display the masks on the radiation patterns')
+plt.show()
+
+
+# SLL
+Theta_s = Theta_fn / 2 + np.radians(4)
+E_S = (u_ - u_0)**2 + (v_ - v_0)**2 - sin(Theta_s)**2
+
+values4 = copy.deepcopy(values1)
+
+values4[np.where(E_S<=0)] = nan
+
+plt.imshow(values4, cmap='jet', origin='lower')
+plt.axis('equal') 
+plt.colorbar(label='')
+plt.axis('off')
+plt.title('Check SLL Area')
+plt.show()
+
+
+# # Plot 2d Radiation Pattern
+# x_asix = np.linspace(-85, 85, N_x)
+# plt.figure()
+# plt.plot(x_asix, values[:, 100], marker='o', linestyle='-', color='b')
+
+# plt.title('2D Radiation Pattern')
+# plt.grid(True)
+# plt.show()
+
+
+# Prepare the data to calculate the directity
+E_2 = copy.deepcopy(E)
+s_1 = copy.deepcopy(s).astype(complex)
+E_2 = E_2**2
+E_2 = E_2 / np.sqrt(s_1)
+E_2[np.isnan(E_2)] = 0
+E_int = trapz(E_2, u)
+E_int = trapz(E_int, v)
+# print('int',E_int)
+E_0 = E[np.where(values==0)]
+print("Max value position: ", np.argwhere(values==0))
+D_0 = abs(4*pi*E_0**2 / E_int)
+print("Directity: ", 10*np.log10(D_0))
+print("SLL: ", nanmax(values4))
+
+
+# Create a DataFrame
+df = pd.DataFrame(values, index=u, columns=v)
+
+# File path
+csv_file_path = 'UNSGA3 Binary_circle.csv'
+
+# Write the DataFrame to a CSV file (including the index)
+df.to_csv(csv_file_path, index=True, header=True)
