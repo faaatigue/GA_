d9c8857a0dde0850794d46eb168ebb5cb64d9719
faaatigue <63487470+faaatigue@users.noreply.github.com>
2024-03-31 23:10:56 +0800

    Update UNSGA3 float_circle_copy.py

--- ah/UNSGA3 float_circle_copy.py
+++ ah/UNSGA3 float_circle_copy.py
@@ -52,9 +52,9 @@ d2 = 100
 theta_r = np.radians(20)
 phi_r = np.radians(0)
 
-Theta_hp = np.radians(20)
-Theta_fn = np.radians(25)
-SLL = -20
+Theta_hp = np.radians(10)
+Theta_fn = np.radians(13)
+SLL = -25
 
 
 
